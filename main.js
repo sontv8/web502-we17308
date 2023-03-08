eb84c2d790955f28f06862787f21357e9b3caf22
sontv8 <sontv8@fpt.edu.vn>
2023-03-08 16:59:58 +0700

    Buổi 2 - thực hành

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 "use strict";
-var username;
-username = "123";
-var a = 10;
-var arrNumber = [1, 2, 3];
-var arrString = ["abc", "def"];
-var arrMix = [1, "abc"];
-var teacher = {
+exports.__esModule = true;
+// let username: string;
+// username = "123";
+// const a: number = 10;
+// const arrNumber: number[] = [1, 2, 3]
+// const arrString: string[] = ["abc", "def"]
+// const arrMix: Array<number | string> = [1, "abc"]
+// const teacher: { id: number, username: string, email: string } = {
+//     id: 1,
+//     username: "datdeptrai",
+//     email: "datlt@gmail.com"
+// }
+// function sum(a: number, b: number): number {
+//     let result = a + b;
+//     return result;
+// }
+// sum(10, 20)
+var project = {
     id: 1,
-    username: "datdeptrai",
-    email: "datlt@gmail.com"
+    name: "Du an mau",
+    image: "http://...",
+    link: "http://...",
+    status: true
 };
-function sum(a, b) {
-    console.log(a + b);
-}
-sum("10", 20);
+var getProject = function (props) {
+    return props;
+};
+var demo = {
+    id: 123
+};
+console.log(getProject(project));
--- main.ts
+++ main.ts
@@ -1,21 +1,37 @@
 export { }
-let username: string;
-username = "123";
+// let username: string;
+// username = "123";
 
-const a: number = 10;
+// const a: number = 10;
 
-const arrNumber: number[] = [1, 2, 3]
-const arrString: string[] = ["abc", "def"]
-const arrMix: Array<number | string> = [1, "abc"]
+// const arrNumber: number[] = [1, 2, 3]
+// const arrString: string[] = ["abc", "def"]
+// const arrMix: Array<number | string> = [1, "abc"]
 
-const teacher: { id: number, username: string, email: string } = {
+// const teacher: { id: number, username: string, email: string } = {
+//     id: 1,
+//     username: "datdeptrai",
+//     email: "datlt@gmail.com"
+// }
+
+// function sum(a: number, b: number): number {
+//     let result = a + b;
+//     return result;
+// }
+// sum(10, 20)
+
+const project: { id: number, name: string, image: string, link: string, status: boolean } = {
     id: 1,
-    username: "datdeptrai",
-    email: "datlt@gmail.com"
+    name: "Du an mau",
+    image: "http://...",
+    link: "http://...",
+    status: true
 }
 
-function sum(a: number, b: number): number {
-    let result = a + b;
-    return result;
+const getProject = (props: { id: number, name: string, image: string, link: string, status: boolean }): { id: number, name: string, image: string, link: string, status: boolean } => {
+    return props
 }
-sum(10, 20)
+const demo = {
+    id: 123
+}
+console.log(getProject(project));
